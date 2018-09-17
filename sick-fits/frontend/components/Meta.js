c949e79ec1b894e91ab9fb2001db2ca62e3b4cff
David Thorpe <davidthorpe878@gmail.com>
2018-09-17 21:27:28 +0100

    End of video 9

---
+++ sick-fits/frontend/components/Meta.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import React from 'react';
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <link rel="shortcut icon" href="/static/favicon.png" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Sick Fits!</title>
+    </Head>
+  );
+};
+
+export default Meta;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      <p>sell!</p>
+    </div>
+  );
+};
+
+export default Sell;
