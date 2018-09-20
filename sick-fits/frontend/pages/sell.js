0fd1c04d8fe11feb171ac9220e22647763ea3aa5
David Thorpe <davidthorpe878@gmail.com>
2018-09-20 21:43:13 +0100

    Lots of updates to client

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import CreateItem from "../components/CreateItem";
 
 const Sell = () => {
   return (
     <div>
       <p>sell!</p>
+      <CreateItem />
     </div>
   );
 };
