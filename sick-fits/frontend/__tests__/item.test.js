097e35d503c27b0b559018743d47ceebdd7db384
David Thorpe <davidthorpe878@gmail.com>
2018-11-08 20:25:21 +0000

    more tests!

--- sick-fits/frontend/__tests__/item.test.js
+++ sick-fits/frontend/__tests__/item.test.js
@@ -11,15 +11,30 @@ const fakeItem = {
 };
 
 describe('<Item />', () => {
-  it('renders and displays properly', () => {
+  it('renders the image properly', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    console.log(wrapper.debug());
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it('renders priceTag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    // console.log(wrapper.debug());
     const PriceTag = wrapper.find('PriceTag');
     expect(PriceTag.children().text()).toBe('£50');
     expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
-    const img = wrapper.find('img');
-    console.log(img.props());
-    expect(img.props().src).toBe(fakeItem.image);
-    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find('.buttonList');
+    // console.log(buttonList.debug());
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('Link').exists()).toBe(true);
+    expect(buttonList.find('Link').exists()).toBeTruthy();
+    expect(buttonList.find('AddToCart').exists()).toBeTruthy();
+    expect(buttonList.find('DeleteItem').exists()).toBeTruthy();
   });
 });
